22ef32dc24371f58cbaa0393e42e9bf5042a4772
BRIGHTZ-ee <wanjayggqq@gmail.com>
2024-08-09 15:03:39 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['628989110808']
+global.botname = '<D A N Z X Y Y - C R A S H - M A K E R>'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "𝐃𝚫𝐍𝐗𝐙𝐘𝐘𝐂𝐑𝚪𝚫𝐒𝐇𝟔𝟔𝟔"
+global.sticker2 = "🐶🚮"
